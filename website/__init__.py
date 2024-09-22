f4740576017aec86b585bb1e7dca21cfee4658b7
tesims <tesims@umich.edu>
2024-09-22 15:45:06 -0400

    Fix Flask-Migrate initialization

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_migrate import Migrate
 from config import DevelopmentConfig, ProductionConfig
 import os
 
@@ -18,6 +19,7 @@ def create_app():
     db.init_app(app)
     login_manager.init_app(app)
     login_manager.login_view = 'auth.login'
+    migrate = Migrate(app, db)
 
     from .views import views
     from .auth import auth
